87a5e2e6f711621bf0160177d10ff913ca0db78b
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-11-21 23:29:41 -0500

    Added addExpenseHandler

--- Expense Tracker Project/ExpenseTracker/src/App.js
+++ Expense Tracker Project/ExpenseTracker/src/App.js
@@ -5,43 +5,27 @@ import ExpensesList from './components/Expenses/ExpensesList/ExpensesList';
 const PLACEHOLDER_EXPENSES = [
   { 
     id:'e1',
-    title: "Toilet Paper", 
-    amount: 94.12, 
-    date: new Date(2020, 7, 14) 
-  },
-  { 
-    id:'e2',
-    title: "Car Insurance", 
-    amount: 294.67, 
-    date: new Date(2021, 2, 28) 
-  },
-  { 
-    id:'e3',
-    title: "New TV", 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12) 
-  },
-  { 
-    id:'e5',
-    title: "Gas", 
-    amount: 46.74, 
-    date: new Date(2021, 7, 26) 
+    title: "Title", 
+    amount: 99.99, 
+    date: new Date(2020, 1, 1) 
   },
 ];
 
 
-const App = (props) => {
+const App = () => {
   const [expenses, setExpenses] = useState(PLACEHOLDER_EXPENSES);
 
 
-  const addExpenseHandler = expense => {
-    setExpenses([expense, ...expenses]);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <ExpensesList items={PLACEHOLDER_EXPENSES}/>
+      <ExpensesList items={expenses}/>
     </div>
   );
 }
