290a7e7e0456c9135679b122180bb2d43fef4aeb
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-11-21 23:08:55 -0500

    Rearanged the folders

--- Expense Tracker Project/ExpenseTracker/src/App.js
+++ Expense Tracker Project/ExpenseTracker/src/App.js
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpensesList from './components/Expenses/ExpensesList/ExpensesList';
 
+const PLACEHOLDER_EXPENSES = [
+  { 
+    id:'e1',
+    title: "Toilet Paper", 
+    amount: 94.12, 
+    date: new Date(2020, 7, 14) 
+  },
+  { 
+    id:'e2',
+    title: "Car Insurance", 
+    amount: 294.67, 
+    date: new Date(2021, 2, 28) 
+  },
+  { 
+    id:'e3',
+    title: "New TV", 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) 
+  },
+  { 
+    id:'e5',
+    title: "Gas", 
+    amount: 46.74, 
+    date: new Date(2021, 7, 26) 
+  },
+];
+
+
 const App = (props) => {
-  const expenses = [
-    { 
-      id:'e1',
-      title: "Toilet Paper", 
-      amount: 94.12, 
-      date: new Date(2020, 7, 14) 
-    },
-    { 
-      id:'e2',
-      title: "Car Insurance", 
-      amount: 294.67, 
-      date: new Date(2021, 2, 28) 
-    },
-    { 
-      id:'e3',
-      title: "New TV", 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
-    },
-    { 
-      id:'e5',
-      title: "Gas", 
-      amount: 46.74, 
-      date: new Date(2021, 7, 26) 
-    },
-  ];
+  const [expenses, setExpenses] = useState(PLACEHOLDER_EXPENSES);
+
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+    setExpenses([expense, ...expenses]);
+  };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <ExpensesList items={expenses}/>
+      <ExpensesList items={PLACEHOLDER_EXPENSES}/>
     </div>
   );
 }
