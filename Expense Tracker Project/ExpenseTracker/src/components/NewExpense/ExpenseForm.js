f650ae436b6b8d10d45658f39509b5ce0d852884
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-11-22 22:52:54 -0500

    Added states for open & close add expense button

--- Expense Tracker Project/ExpenseTracker/src/components/NewExpense/ExpenseForm.js
+++ Expense Tracker Project/ExpenseTracker/src/components/NewExpense/ExpenseForm.js
@@ -61,7 +61,8 @@ const ExpenseForm = (props) => {
         </div>
     </div>
     <div className="new-expense__actions">
-        <button type="submit" >Add Expenses</button>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
+        <button type="submit" >Add Expense</button>
     </div>
   </form>
   );
--- Expense Tracker Project/ExpenseTracker/src/components/NewExpense/NewExpense.js
+++ Expense Tracker Project/ExpenseTracker/src/components/NewExpense/NewExpense.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [addClicked, setAddClicked] = useState(false);
+
+
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData ={
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setAddClicked(false);
+    }
+
+    const addClickedHandler = () => {
+        setAddClicked(true);
+    };
+
+    const stopAddHandler = () => {
+        setAddClicked(false);
     }
 
     return(
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+            {!addClicked && <button onClick={addClickedHandler}>Add New Expense</button>}
+            {addClicked && 
+            <ExpenseForm 
+                onSaveExpenseData={onSaveExpenseDataHandler}
+                onCancel={stopAddHandler}/>}
         </div>
     )
 }
