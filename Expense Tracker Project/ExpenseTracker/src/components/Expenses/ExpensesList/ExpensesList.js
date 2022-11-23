e7e2e21f42d57ccf02bec54bd96a3a80557f5e4d
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-11-22 22:33:56 -0500

    Added Some Conditional Output Logic

--- Expense Tracker Project/ExpenseTracker/src/components/Expenses/ExpensesList/ExpensesList.js
+++ Expense Tracker Project/ExpenseTracker/src/components/Expenses/ExpensesList/ExpensesList.js
@@ -1,33 +1,33 @@
-import React, {useState} from "react";
-import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import React, { useState } from "react";
 import Card from "../../UI/Card/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
-import './ExpensesList.css';
+import "./ExpensesList.css";
+import ExpensesLists from "../ExpensesLists/ExpensesLists";
 
 const ExpensesList = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-    const filterChangeHandler = selectedYear => {
-        setFilteredYear(selectedYear);
-      };
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const filteredExpenses = props.items.filter(expense => {
-        return expense.date.getFullYear().toString() === filteredYear;
-    });
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-    return(
-        <div>
-            <Card className='expenses'>
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {filteredExpenses.map((expense) => 
-            <ExpenseItem 
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}/>)}
-            </Card>
-        </div>
-    )
-}
+  
+
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+       <ExpensesLists items={filteredExpenses}/>
+      </Card>
+    </li>
+  );
+};
 
 export default ExpensesList;
